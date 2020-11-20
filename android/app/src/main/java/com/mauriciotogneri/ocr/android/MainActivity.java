2f9547abf49b36918ba0f9339f6332b9e80c0bd1
Mauricio Togneri <mauricio.togneri@gmail.com>
2020-11-20 14:57:38 +0100

    Showing camera frames

--- android/app/src/main/java/com/mauriciotogneri/ocr/android/MainActivity.java
+++ android/app/src/main/java/com/mauriciotogneri/ocr/android/MainActivity.java
@@ -1,15 +1,96 @@
 package com.mauriciotogneri.ocr.android;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    private static final int REQUEST_CAMERA_PERMISSION = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        if (cameraPermissionGranted())
+        {
+            startCamera();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, new String[] {permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        }
+
+        findViewById(R.id.button_capture).setOnClickListener(view -> takePhoto());
+    }
+
+    private void startCamera()
+    {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try
+            {
+                PreviewView previewView = findViewById(R.id.preview);
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void takePhoto()
+    {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CAMERA_PERMISSION)
+        {
+            if (cameraPermissionGranted())
+            {
+                startCamera();
+            }
+        }
+    }
+
+    private boolean cameraPermissionGranted()
+    {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        executor.shutdown();
     }
 }
