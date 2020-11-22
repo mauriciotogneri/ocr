996c7eaf91d656cf129c1576569bd24cff943efc
Mauricio Togneri <mauricio.togneri@gmail.com>
2020-11-22 12:46:18 +0100

    Extracting text

--- flutter/lib/screens/main_screen.dart
+++ flutter/lib/screens/main_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:typed_data';
 import 'package:camera/camera.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -30,11 +32,30 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Future _processCameraImage(CameraImage image) async {
-    final FirebaseVisionImage visionImage =
-        FirebaseVisionImage.fromBytes(image.planes[0].bytes, null);
-    final TextRecognizer recognizeText =
+    /*final FirebaseVisionImageMetadata metadata = FirebaseVisionImageMetadata(
+      rawFormat: image.format.raw,
+      size: Size(image.width.toDouble(), image.height.toDouble()),
+      rotation: ImageRotation.rotation90,
+      planeData: image.planes
+          .map((currentPlane) => FirebaseVisionImagePlaneMetadata(
+              bytesPerRow: currentPlane.bytesPerRow,
+              height: currentPlane.height,
+              width: currentPlane.width))
+          .toList(),
+    );*/
+
+    final ImageRotation rotation = rotationIntToImageRotation(
+      _camera.description.sensorOrientation,
+    );
+
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromBytes(
+      concatenatePlanes(image.planes),
+      buildMetaData(image, rotation),
+    );
+    final TextRecognizer textRecognizer =
         FirebaseVision.instance.textRecognizer();
-    final VisionText readText = await recognizeText.processImage(visionImage);
+    final VisionText readText = await textRecognizer.processImage(visionImage);
+
     String result = '';
     for (final TextBlock block in readText.blocks) {
       for (final TextLine line in block.lines) {
@@ -43,6 +64,47 @@ class _MainScreenState extends State<MainScreen> {
     }
 
     print(result);
+  }
+
+  Uint8List concatenatePlanes(List<Plane> planes) {
+    final WriteBuffer allBytes = WriteBuffer();
+    planes.forEach((plane) => allBytes.putUint8List(plane.bytes));
+
+    return allBytes.done().buffer.asUint8List();
+  }
+
+  FirebaseVisionImageMetadata buildMetaData(
+    CameraImage image,
+    ImageRotation rotation,
+  ) {
+    return FirebaseVisionImageMetadata(
+      rawFormat: image.format.raw,
+      size: Size(image.width.toDouble(), image.height.toDouble()),
+      rotation: rotation,
+      planeData: image.planes.map(
+        (plane) {
+          return FirebaseVisionImagePlaneMetadata(
+            bytesPerRow: plane.bytesPerRow,
+            height: plane.height,
+            width: plane.width,
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  ImageRotation rotationIntToImageRotation(int rotation) {
+    switch (rotation) {
+      case 0:
+        return ImageRotation.rotation0;
+      case 90:
+        return ImageRotation.rotation90;
+      case 180:
+        return ImageRotation.rotation180;
+      default:
+        assert(rotation == 270);
+        return ImageRotation.rotation270;
+    }
   }
 
   @override
